2a68259d28d0346507db0ac73054136726c0910d
TawitBurinkul <zentawit@gmail.com>
2024-05-24 15:37:13 +0700

    fix dialogue box problem

--- TiemEngine/GameStateController.cpp
+++ TiemEngine/GameStateController.cpp
@@ -41,7 +41,7 @@ void GameStateController::LoadLevel()
 	switch (gameStateCurr)
 	{
 		case GameState::GS_LEVEL1:
-			currentLevel = new Level2Scene5p3();
+			currentLevel = new Level2();
 			break;
 		case GameState::GS_LEVEL2:
 			currentLevel = new Level2();
